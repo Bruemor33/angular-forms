1ee7f6ce9658f93f140d73ffcac25950de7d125c
Andrew Rymer <Andrew.Rymer@acacceptance.com>
2019-09-24 16:14:50 -0400

    Constructed app.module class

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: 'welcome',
+                component: WelcomeComponent
+            }
+        ])
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+
+export class AppModule { }
